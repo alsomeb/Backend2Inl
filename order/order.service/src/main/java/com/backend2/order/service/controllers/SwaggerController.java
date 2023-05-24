3843ceace2e94ebac3122b9ba8a9d03ff1fb88ca
alsomeb <alsomeb@gmail.com>
2023-05-24 15:03:44 +0200

    - added SpringFox Swagger Api Doc - Redirects from "/" on order-service root url

---
+++ order/order.service/src/main/java/com/backend2/order/service/controllers/SwaggerController.java
@@ -0,0 +1,12 @@
+package com.backend2.order.service.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerController {
+    @GetMapping("/")
+    public String redirectSwagger() {
+        return "redirect:/swagger-ui.html";
+    }
+}
